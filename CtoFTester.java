e694f9548f0cbb46b11ee080825c0d07301a264b
sbrink00 <sbrink00@stuy.edu>
2018-09-18 21:18:01 -0400

    I created the main function to run both smaller functions

---
+++ CtoFTester.java
@@ -0,0 +1,21 @@
+public class CtoFTester{
+  public static double tester(){
+
+    System.out.println(celsiusToFahrenheit(0));
+    System.out.println(celsiusToFahrenheit(15));
+    System.out.println(celsiusToFahrenheit(-50));
+    System.out.println(celsiusToFahrenheit(-40));
+    System.out.println(fahrenheitToCelsius(0));
+    System.out.println(fahrenheitToCelsius(15));
+    System.out.println(fahrenheitToCelsius(-50));
+    System.out.println(fahrenheitToCelsius(-40));
+  }
+
+  public static double celsiusToFahrenheit(int a){
+
+  }
+
+  public static double fahrenheitToCelsius(int a){
+    
+  }
+}
